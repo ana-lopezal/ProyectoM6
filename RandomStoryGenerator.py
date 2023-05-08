34942b8b3f6cbb759b5a4408a72ac6f9e3de5e81
alexamarinvm13 <132517406+alexamarinvm13@users.noreply.github.com>
2023-05-08 15:21:22 -0600

    Update RandomStoryGenerator.py

--- RandomStoryGenerator.py
+++ RandomStoryGenerator.py
@@ -3,4 +3,8 @@ Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time']
  
 character = [' there lived a king.',' there was a man named Jack.',
              ' there lived a farmer.']
+
+time = [' One day', ' One full-moon night']
+  
+story_plot = [' he was passing by', ' he was going for a picnic to ']
   
